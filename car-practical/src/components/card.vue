059f0faf35f92d3a1c99ca19fdddc31849b826c0
prem <prem.panchal@simformsolutions.com>
2023-04-25 13:12:24 +0530

    feat:add dynamic cards and navbar

---
+++ car-practical/src/components/card.vue
@@ -0,0 +1,14 @@
+<template>
+  <card :cars="cars"></card>
+</template>
+
+<script>
+import card from "./card.vue";
+
+export default {
+  name: "App",
+  components: {
+    card,
+  },
+};
+</script>
