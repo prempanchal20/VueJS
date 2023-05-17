17e730c6e10c771ba240daea72f5020c10151603
prem <prem.panchal@simformsolutions.com>
2023-05-17 15:25:38 +0530

    take practical-4 changes

---
+++ car_showroom-5/src/components/Navbar.vue
@@ -0,0 +1,55 @@
+<template>
+  <section class="header-go">
+    <div class="header-content">
+      <div class="header-logo">
+        <a href="">Car Showroom</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  text-decoration: none;
+  list-style: none;
+}
+
+.header-go {
+  position: sticky;
+  top: 0;
+  width: 100%;
+  background-color:#334756;
+  z-index: 1;
+}
+
+.header-content {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px 30px;
+}
+
+.header-logo a {
+  font-size: 24px;
+  font-weight: bold;
+  color: #fff;
+  border: 1px solid white;
+  padding: 10px;
+  border-radius: 10px;
+}
+
+.menu-icon {
+  display: none;
+}
+
+#menu-toggle {
+  display: none;
+}
+
+#menu-toggle:checked~.menu {
+  transform: scale(1, 1);
+}
+</style>
---
+++ car_showroom-5/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <h3>Home Components</h3>
+</template>
+  
+<script>
+export default {
+    data() {
+        return {
+            post: []
+        }
+    },
+
+    methods: {
+        carsData() {
+            fetch(
+                "https://testapi.io/api/dartya/resource/cardata"
+            ).catch((error) => alert("Coudn't call the GET API... Please try Again"))
+                .then(response => response.json())
+                .then(data => this.post = data)
+        },
+    },
+    mounted() {
+        this.getPosts()
+    }
+}
+</script>
